5843aae24f1969549882b2e64b620cb9eb2db64d
Israel Viveros <israelviverosdev@gmail.com>
2017-05-18 11:12:19 -0500

    Adding editor config and JS linter

--- app.js
+++ app.js
@@ -9,28 +9,28 @@ import routes from "./lib/routes";
 let app = express();
 
 app.use(cors())
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({extended: true}))
-    .use('/', routes);
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({extended: true}))
+  .use('/', routes);
 
 if (app.get('env') !== 'test') {
-    app.use(logger('dev'));
+  app.use(logger('dev'));
 }
 
 
 app.use(function (req, res) {
-    res.status(404).json({error: true, message: messages.es.notfound});
+  res.status(404).json({error: true, message: messages.es.notfound});
 });
 
 
 if (app.get('env') === 'development') {
-    app.use((err, req, res) => {
-        res.status(err.status || 500).json({error: true, message: err.message});
-    });
+  app.use((err, req, res) => {
+    res.status(err.status || 500).json({error: true, message: err.message});
+  });
 }
 
 app.use((err, req, res) => {
-    res.status(err.status || 500).json({error: true, message: error.message});
+  res.status(err.status || 500).json({error: true, message: error.message});
 });
 
 
